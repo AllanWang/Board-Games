16150fd43196c01d1b768d0030342d56a092dfed
Allan Wang <me@allanwang.ca>
2025-04-18 01:10:20 -0700

    Include compose runtime for shared package

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,7 +3,8 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
   alias(libs.plugins.kotlinMultiplatform)
-
+  alias(libs.plugins.composeMultiplatform)
+  alias(libs.plugins.composeCompiler)
 }
 
 kotlin {
@@ -28,6 +29,7 @@ kotlin {
 
   sourceSets {
     commonMain.dependencies {
+      implementation(compose.runtime)
       // put your Multiplatform dependencies here
     }
   }
