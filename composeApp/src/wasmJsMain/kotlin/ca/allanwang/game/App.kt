90da7d9da33d0ba8533cce6c3a1074229b519748
Allan Wang <me@allanwang.ca>
2025-04-19 15:00:00 -0700

    Use material3

--- composeApp/src/wasmJsMain/kotlin/ca/allanwang/game/App.kt
+++ composeApp/src/wasmJsMain/kotlin/ca/allanwang/game/App.kt
@@ -8,9 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
